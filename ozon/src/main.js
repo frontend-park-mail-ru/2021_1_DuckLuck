cef969abc650913a4bb74848fe62d3f610577fa8
Denis K <ldv.levart@gmail.com>
2021-03-07 22:30:46 +0300

    Initial version of avatar loader.

--- ozon/src/main.js
+++ ozon/src/main.js
@@ -106,7 +106,32 @@ config.me.open = () => {
             const avatar = application.getElementsByClassName('profile-info__user-avatar-input')[0];
             application.addEventListener('submit', (evt) => {
                 evt.preventDefault();
-                console.log(avatar.files[0]);
+
+                        let reader = new FileReader();
+
+                        // Closure to capture the file information.
+                        reader.onload = (function(theFile) {
+                            return function(e) {
+                                let avatar = e.target.result;
+                                // https://developer.mozilla.org/en/JavaScript_typed_arrays
+                                // let avatar = new Uint8Array(raw);
+                                AjaxModule.putUsingFetch({
+                                    url: 'http://localhost:8080/api/v1/user/profile/avatar',
+                                    body: {avatar},
+                                }).then(({status, parsedJson}) => {
+                                        console.log(status, parsedJson);
+                                    }
+                                )
+
+                                console.log(avatar);
+                            };
+                        })(application.getElementsByClassName('profile-info__user-avatar-input')[0].files[0]);
+
+
+                        // console.log(application.getElementsByClassName('profile-info__user-avatar-input')[0].files[0]);
+                        // Read in the image file as a data URL.
+                        reader.readAsBinaryString(application.getElementsByClassName('profile-info__user-avatar-input')[0].files[0]);
+
             });
 
         })
