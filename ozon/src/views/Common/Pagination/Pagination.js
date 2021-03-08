41f77a0ccf32d1f87739e4246dbaeecadbcd41ac
DuckLuckBreakout <mrdivan07@gmail.com>
2021-03-08 12:54:55 +0300

    OZON-10 Add pagination

---
+++ ozon/src/views/Common/Pagination/Pagination.js
@@ -0,0 +1,39 @@
+import {Button} from "../Button/Button.js"
+import PaginationTemplate from "./Pagination.hbs"
+
+export class Pagination {
+    constructor({pagesCount, currentPage}) {
+        this.pagesCount = pagesCount;
+        this.currentPage = currentPage;
+    }
+
+    getHtmlString = () => {
+        const buttons = [];
+
+        if (this.currentPage > 2) {
+            buttons.push(new Button({name: 'button_pagination_first', value: '1'}));
+        }
+
+        for (let i = this.currentPage - 2; i <= this.currentPage + 2; i++) {
+            if (i <= 0) {
+                continue;
+            }
+            if (i > this.pagesCount) {
+                break;
+            }
+            let buttonName;
+            if (i === this.currentPage) {
+               buttonName = 'button_pagination_current-page';
+            } else {
+               buttonName = 'button_pagination_common';
+            }
+            buttons.push(new Button({name: buttonName, value: i.toString()}));
+        }
+
+        if (this.pagesCount > this.currentPage + 2) {
+            buttons.push(new Button({name: 'button_pagination_last', value: this.pagesCount.toString()}));
+        }
+
+        return PaginationTemplate({buttons: buttons});
+    }
+}
