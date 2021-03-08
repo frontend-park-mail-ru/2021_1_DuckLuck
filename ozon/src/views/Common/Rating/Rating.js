e7f0cd9f8f039970cf034a2c64cd34cda4489fdc
DuckLuckBreakout <mrdivan07@gmail.com>
2021-03-08 20:44:23 +0300

    OZON-40 Add product page

--- ozon/src/views/Common/Rating/Rating.js
+++ ozon/src/views/Common/Rating/Rating.js
@@ -1,7 +1,7 @@
 import RatingTempalte from "./Rating.hbs"
 
 export class Rating {
-    constructor({minRating = 0, maxRating = 5}) {
+    constructor({minRating = 0, maxRating = 5} = {}) {
         this.objectType = 'rating';
         this.minRatingValue = minRating;
         this.maxRatingValue = maxRating;
