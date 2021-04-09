441259afe18afa826571452da19ae5c99402d8b9
DuckLuckBreakout <49189299+DuckLuckBreakout@users.noreply.github.com>
2021-04-09 18:27:35 +0300

    OZON-32 Add header presenter

---
+++ ozon/src/presenters/HeaderPresenter.js
@@ -0,0 +1,49 @@
+import BasePresenter from './BasePresenter.js';
+import Events from '../utils/bus/events';
+import Responses from '../utils/bus/responses';
+
+/**
+ * @description Presenter for Header View and Model
+ */
+class HeaderPresenter extends BasePresenter {
+    /**
+     * @param {HTMLElement} application html of application
+     * @param {Class} View Class of view object
+     * @param {Class} Model Class of model object
+     */
+    constructor(application, View, Model) {
+        super(application, View, Model);
+        this.bus.on(Events.HeaderLoad, this.loadHeader);
+        this.bus.on(Events.HeaderLoaded, this.headerLoadedReaction);
+    }
+
+    /**
+     *
+     * @return {Object}
+     */
+    get categories() {
+        return this.model.categories;
+    }
+
+    /**
+     *
+     */
+    loadHeader = () => {
+        this.model.loadHeader();
+    }
+
+    /**
+     *
+     * @param {string}result
+     */
+    headerLoadedReaction = (result) => {
+        if (result === Responses.Success) {
+            this.view.reRender();
+            this.view.cache.hidden = false;
+        } else {
+            console.error('Cant load header');
+        }
+    }
+}
+
+export default HeaderPresenter;
