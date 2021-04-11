80a2971a43b4c95104fd63d16e26d40a45623c19
DuckLuckBreakout <49189299+DuckLuckBreakout@users.noreply.github.com>
2021-04-11 16:49:39 +0300

    OZON-32 Fix header searching

--- ozon/src/views/HeaderView/HeaderView.js
+++ ozon/src/views/HeaderView/HeaderView.js
@@ -86,7 +86,7 @@ export class HeaderView extends BaseView {
             buttonStyles: buttonStyles,
         });
 
-        this.cache = new DOMParser().parseFromString(template, 'text/html').getElementById('header');
+        this.cache = new DOMParser().parseFromString(template, 'text/html').getElementsByClassName(headerStyles.main);
 
         const logoButton = this.cache.getElementsByClassName(headerStyles.logoBlock)[0];
         logoButton.addEventListener('click', () => {
