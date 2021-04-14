4459a376efd5b6b45054285438a6e24bad53591b
DuckLuckBreakout <49189299+DuckLuckBreakout@users.noreply.github.com>
2021-04-14 03:28:41 +0300

    OZON-48 Add styles for order

--- ozon/src/views/OrderView/OrderView.js
+++ ozon/src/views/OrderView/OrderView.js
@@ -2,6 +2,7 @@ import {BaseView} from '../BaseView.js';
 import Events from '../../utils/bus/events';
 import orderTemplate from './OrderView.hbs';
 import {Input} from '../Common/Input/Input';
+import orderStyles from './OrderView.css';
 
 /**
  * @class ProductsView
@@ -17,19 +18,20 @@ export class OrderView extends BaseView {
         this.parent.innerHTML = '';
         const template = orderTemplate({
             productsList: this.presenter.products,
-            inputFields: [new Input({type: 'email', name: 'email', placeholder: 'Email address',
+            inputFields: [new Input({type: 'email', name: 'email', placeholder: 'Электронная почта',
                 value: this.presenter.recipient.email}),
-            new Input({type: 'text', name: 'firstName', placeholder: 'First Name',
+            new Input({type: 'text', name: 'firstName', placeholder: 'Имя',
                 value: this.presenter.recipient.first_name}),
-            new Input({type: 'text', name: 'lastName', placeholder: 'Last name',
+            new Input({type: 'text', name: 'lastName', placeholder: 'Фамилия',
                 value: this.presenter.recipient.last_name}),
-            new Input({type: 'text', name: 'address', placeholder: 'Delivery Address'})],
+            new Input({type: 'text', name: 'address', placeholder: 'Адрес доставки'})],
             price: this.presenter.price,
+            orderStyles: orderStyles,
         });
         this.cache = new DOMParser().parseFromString(template, 'text/html').getElementById('products-list-block');
         this.parent.appendChild(this.cache);
 
-        document.getElementById('form').addEventListener('submit', (evt) => {
+        this.cache.getElementsByClassName(orderStyles.orderButton)[0].addEventListener('click', (evt) => {
             evt.preventDefault();
             this.bus.emit(Events.SendOrder);
         });
