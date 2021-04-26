c5f74bbdec8a737c526b4b028be1fbb75e015548
DuckLuckBreakout <49189299+DuckLuckBreakout@users.noreply.github.com>
2021-04-26 23:53:34 +0300

    OZON-100 Add review model

---
+++ ozon/src/models/ReviewModel.js
@@ -0,0 +1,116 @@
+import {AjaxModule} from '../modules/Ajax/Ajax';
+import {serverApiPath, urls} from '../utils/urls/urls';
+import BaseModel from './BaseModel';
+import Events from '../utils/bus/events';
+import Responses from '../utils/bus/responses';
+import HTTPResponses from '../utils/http-responses/httpResponses';
+import Router from '../utils/router/Router';
+
+/**
+ * @description Model for Review in MVP Arch
+ */
+class ReviewModel extends BaseModel {
+    #productId
+    #rating
+    #advantages
+    #disadvantages
+    #comment
+    #isPublic
+
+    /**
+     * @return {Number} id of product
+     */
+    get productId() {
+        return this.#productId;
+    }
+
+    /**
+     * @return {Number} rating
+     */
+    get rating() {
+        return this.#rating;
+    }
+
+    /**
+     * @return {string} advantages
+     */
+    get advantages() {
+        return this.#advantages;
+    }
+
+    /**
+     * @return {string} disadvantages
+     */
+    get disadvantages() {
+        return this.#disadvantages;
+    }
+
+    /**
+     * @param {Number} id of product
+     */
+    set productId(id) {
+        this.#productId = id;
+    }
+
+    /**
+     * @param {Number} rating
+     */
+    set rating(rating) {
+        this.#rating = rating;
+    }
+
+    /**
+     * @param {string} advantages
+     */
+    set advantages(advantages) {
+        this.#advantages = advantages;
+    }
+
+    /**
+     * @param {string} disadvantages
+     */
+    set disadvantages(disadvantages) {
+        this.#disadvantages = disadvantages;
+    }
+
+    /**
+     * @description Loads all information about review via AJAX
+     */
+    loadReview = () => {
+        // TODO: проверка на то, что пользователь может оставлять отзыв к данному товару
+        this.bus.emit(Events.OrderLoaded, Responses.Success);
+    }
+
+    /**
+     * @description Sends review form to backend
+     */
+    sendReview = () => {
+        AjaxModule.postUsingFetch({
+            url: serverApiPath + urls.order,
+            body: {
+                product_id: this.#productId,
+                rating: this.#rating,
+                advantages: this.#advantages,
+                disadvantages: this.#disadvantages,
+                comment: this.#comment,
+                isPublic: this.#isPublic,
+            },
+        }).then((response) => {
+            if (response.status !== HTTPResponses.Success) {
+                throw response.status;
+            }
+        }).catch((err) => {
+            switch (err) {
+            case HTTPResponses.Offline: {
+                Router.open('/offline');
+                break;
+            }
+            default: {
+                console.error('error review sending');
+            }
+            }
+        });
+    }
+}
+
+export default ReviewModel;
